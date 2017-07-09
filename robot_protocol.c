fa61bd25f772fe93b83aba0e6579ee1bff86b38c
HeoHeo <h0h6h2h5@gmail.com>
2017-07-09 22:31:13 +0900

    Add BIOLOID_PACKET

--- robot_protocol.c
+++ robot_protocol.c
@@ -1,9 +1,26 @@
+#include <stdlib.h>
 #include "robot_protocol.h"
 
 
 #define _UART_BAUD_RATE		57600
 
+typedef struct _BIOLOID_PACKET {
+	unsigned char	startCode0;		// start code 0			(1byte)
+	unsigned char	startCode1;		// start code 1			(1byte)
+	unsigned char	dataLow0;		// low data				(1byte)
+	unsigned char	dataLow1;		// inverted low data	(1byte)
+	unsigned char	dataHigh0;		// high data			(1byte)
+	unsigned char	dataHigh1;		// inverted high data	(1byte)
+} BIOLOID_PACKET, *LPBIOLOID_PACKET;
+
+LPBIOLOID_PACKET _createPacket(void);
+
 
 void sendDataToRobot(short data) {
 
+}
+
+LPBIOLOID_PACKET _createPacket(void) {
+	LPBIOLOID_PACKET packet	= malloc(sizeof(BIOLOID_PACKET));
+	return packet;
 }
