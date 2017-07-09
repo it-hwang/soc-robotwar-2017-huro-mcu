659ff7f7579ed43435fa099be5124b0d5c568fca
HeoHeo <h0h6h2h5@gmail.com>
2017-07-09 22:22:50 +0900

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void displayLogo(void) {
+  	printf("                                                              \n");
+  	printf("                                                              \n");
+  	printf("           *****************************************          \n");
+  	printf("                             Grobot                           \n");
+  	printf("             Welcome to Eagle Robot Platform Board            \n");
+	printf("           *****************************************          \n");
+	printf("                                                              \n");
+}
+
+int main(void)
+{
+	displayLogo();
+
+	return 0;
+}
+
+
