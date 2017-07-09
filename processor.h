b6794b0b7ecbeff0b8ee6c1b6c3c205c92bb2031
HeoHeo <h0h6h2h5@gmail.com>
2017-07-10 07:20:12 +0900

    Add processor

--- processor.h
+++ processor.h
@@ -1,15 +1,18 @@
 #ifndef __PROCESSOR_H__
 #define __PROCESSOR_H__
 
-#define PROCESSOR_GRAPHIC_ERROR			0x01
-#define PROCESSOR_ROBOT_PORT_ERROR		0x02
-#define PROCESSOR_PROCESS_ERROR			0x03
+#define PROCESSOR_CHECKER_PATH			"./main_processor.tmp"
 
-int     openProcessor(void);
-void    closeProcessor(void);
+#define PROCESSOR_FILE_ERROR			0x01
+#define PROCESSOR_GRAPHIC_ERROR			0x02
+#define PROCESSOR_ROBOT_PORT_ERROR		0x03
+#define PROCESSOR_PROCESS_ERROR			0x04
 
-int 	startProcessor(void);
+int		openProcessor(void);
+void	closeProcessor(void);
+
+int		startProcessor(void);
 void	stopProcessor(void);
-BOOL	isProcessorStarted(void);
+int		isProcessorStarted(void);
 
 #endif //__PROCESSOR_H__
