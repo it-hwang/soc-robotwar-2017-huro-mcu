41553e35a5f05e7c3ed1a5e0b3764d1604b9d4a6
HeoHeo <h0h6h2h5@gmail.com>
2017-07-10 09:54:05 +0900

    Remove file system

--- processor.c
+++ processor.c
@@ -1,79 +1,30 @@
 #include <stdio.h>
-#include <unistd.h>
 
 #include "processor.h"
 #include "graphic_api.h"
 #include "robot_protocol.h"
 
-int		_isCheckerExists(void);
-int		_createChecker(int pid);
-void	_removeChecker(void);
-int		_getProcessId(void);
-
-void	_loop(void);
-
 
 int openProcessor(void) {
-	if (open_graphic() < 0)
+	if (open_graphic() < 0) {
+		closeProcessor();
 		return PROCESSOR_GRAPHIC_ERROR;
-	if (openRobotPort() < 0)
+	}
+	if (openRobotPort() < 0) {
+		closeProcessor();
 		return PROCESSOR_ROBOT_PORT_ERROR;
-	
+	}
+
 	return 0;
 }
 
 void closeProcessor(void) {
-	_removeChecker();
 	closeRobotPort();
 	close_graphic();
 }
 
-int startProcessor(void) {
+int runProcessor(void) {
+	printf("run!\n");
+
 	return 0;
-}
-
-void stopProcessor(void) {
-
-}
-
-int isProcessorStarted(void) {
-	return 0;
-}
-
-
-int _isCheckerExists(void) {
-	return access(PROCESSOR_CHECKER_PATH, F_OK) == 0;
-}
-
-int _createChecker(int pid) {
-	FILE* pFile = fopen(PROCESSOR_CHECKER_PATH, "w");
-	if (pFile == NULL)
-		return -1;
-	
-	fprintf(pFile, "%d", pid);
-	fclose(pFile);
-	return 0;
-}
-
-void _removeChecker(void) {
-	if (_isCheckerExists())
-		remove(PROCESSOR_CHECKER_PATH);
-}
-
-int _getProcessId(void) {
-	if (!_isCheckerExists())
-		return 0;
-	
-	FILE* pFile = fopen(PROCESSOR_CHECKER_PATH, "r");
-	if (pFile == NULL)
-		return -1;
-
-	int pid;
-	fscanf(pFile, "%d", &pid);
-	fclose(pFile);
-	return pid;
-}
-
-void _loop(void) {
-
 }
