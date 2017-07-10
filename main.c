01980a441129e6e3808470e1574309de89b7c231
HeoHeo <h0h6h2h5@gmail.com>
2017-07-10 09:54:56 +0900

    Add _blockRunning()

--- main.c
+++ main.c
@@ -1,61 +1,42 @@
-#include <pthread.h>
 #include <stdio.h>
-#include "graphic_api.h"
-#include "robot_protocol.h"
+#include <unistd.h>
 
-#define _ERROR_UNABLE_TO_OPEN_GRAPHIC		0x01
-#define _ERROR_UNABLE_TO_OPEN_ROBOT_PORT	0x02
-#define _ERROR_UNABLE_TO_CREATE_THREAD		0x03
+#include "processor.h"
+#include "terminal.h"
+
+#define _BLOCK_TIMEOUT_MILLISECONDS		3000
 
 int		main(void);
 
-int		_initialize(void);
-void	_finalize(void);
-
-void	_loop(void* data);
-void	_startMainThread(void);
-void	_stopMainThread(void);
-pthread_t	_mainThread;
-int			_mainThreadId;
-
 void	_displayLogo(void);
-
+int		_blockRunning(unsigned int milliseconds);
 
 
 int main(void)
 {
-	int errorCode;
-
 	_displayLogo();
 
-	errorCode = _initialize();
-	if (errorCode == _ERROR_UNABLE_TO_OPEN_GRAPHIC) {
+	if (_blockRunning(_BLOCK_TIMEOUT_MILLISECONDS)) {
+		printf("Program is interrupted.\n");
+		return 0;
+	}
+	printf("Start program.\n");
+
+	int errorCode;
+	errorCode = openProcessor();
+	if (errorCode == PROCESSOR_GRAPHIC_ERROR) {
 		printf("[Error] Unable to open graphic.\n");
 		return 1;
 	}
-	else if (errorCode == _ERROR_UNABLE_TO_OPEN_ROBOT_PORT) {
+	else if (errorCode == PROCESSOR_ROBOT_PORT_ERROR) {
 		printf("[Error] Unable to open robot port.\n");
 		return 1;
 	}
 
-	_finalize();
+	runProcessor();
+	closeProcessor();
+
 	return 0;
-}
-
-
-int _initialize(void) {
-	if (open_graphic() < 0)
-		return _ERROR_UNABLE_TO_OPEN_GRAPHIC;
-	
-	if (openRobotPort() < 0)
-		return _ERROR_UNABLE_TO_OPEN_ROBOT_PORT;
-	
-	return 0;
-}
-
-void _finalize(void) {
-	closeRobotPort();
-	close_graphic();
 }
 
 
@@ -67,4 +48,47 @@ void _displayLogo(void) {
 	printf("             Welcome to Eagle Robot Platform Board            \n");
 	printf("           *****************************************          \n");
 	printf("                                                              \n");
+}
+
+int _blockRunning(unsigned int milliseconds) {
+	printf("Start program after %d seconds.\n", milliseconds);
+	printf("1. Start immediately\n");
+	printf("If you want to interrupt, please press any key except '1' key.\n");
+	printf("\n");
+
+	setConioTerminalMode();
+	while (milliseconds > 0) {
+		if (kbhit()) {
+			if (getch() == '1') {
+				resetTerminalMode();
+				return 0;
+			}
+			else {
+				resetTerminalMode();
+				return 1;
+			}
+		}
+
+		if (milliseconds % 1000 == 0) {
+			resetTerminalMode();
+			printf("Starting %d seconds before ...\n", milliseconds / 1000);
+			setConioTerminalMode();
+		}
+		usleep(0);
+		milliseconds -= 20;
+	}
+
+	if (kbhit()) {
+		if (getch() == '1') {
+			resetTerminalMode();
+			return 0;
+		}
+		else {
+			resetTerminalMode();
+			return 1;
+		}
+	}
+
+	resetTerminalMode();
+	return 0;
 }
