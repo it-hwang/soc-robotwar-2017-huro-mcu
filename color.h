1690819e6455e5c155211391440c5f708a63e8f8
HeoHeo <h0h6h2h5@gmail.com>
2017-07-11 19:34:12 +0900

    Add COLOR

--- color.h
+++ color.h
@@ -1,6 +1,17 @@
 #ifndef __COLOR_H__
 #define __COLOR_H__
 
+#include <stdint.h>
+
+
+typedef enum {
+    COLOR_BLACK,
+    COLOR_WHITE,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_BLUE,
+    COLOR_YELLOW
+} COLOR;
 
 
 
