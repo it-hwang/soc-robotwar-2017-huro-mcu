4d58725b980fd048adefeade99ce9aded7769f04
HeoHeo <h0h6h2h5@gmail.com>
2017-07-16 22:47:17 +0900

    Write launcher script

---
+++ tools/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [[ ! $1 =~ ^COM[0-9]+$ ]]
+then
+    echo "Invalid parameter#1: Please check port number."
+    exit 0
+fi
+portNo=${1:3}
+let ttyNo=$portNo-1
+devName="/dev/ttyS$ttyNo"
+
+if [[ ! $2 =~ ^[0-9]+$ ]]
+then
+    echo "Invalid parameter#2: Please check speed."
+    exit 0
+fi
+speed=$2
+
+stty -F $devName $speed -parity cs8 -cstopb
+echo "root" > $devName
+echo "/mnt/f0/main" > $devName
