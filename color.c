4f3b1c337b804713afb911a5642dea826bb2825a
HeoHeo <h0h6h2h5@gmail.com>
2017-07-19 21:09:06 +0900

    Add COLOR_USE_TYPE_CHECKING

--- color.c
+++ color.c
@@ -9,69 +9,75 @@ ColorTable_t _createColorTable(uint64_t length, Color_t (*pFunc)(uint32_t));
 
 
 void initColorLib(void) {
-	_registerColor(COLOR_BLACK, 0x00, 0x00, 0x00);
-	_registerColor(COLOR_WHITE, 0xff, 0xff, 0xff);
-	_registerColor(COLOR_RED, 0xff, 0x00, 0x00);
-	_registerColor(COLOR_GREEN, 0x00, 0xff, 0x00);
-	_registerColor(COLOR_BLUE, 0x00, 0x00, 0xff);
-	_registerColor(COLOR_YELLOW, 0xff, 0xff, 0x00);
-	_registerColor(COLOR_ORANGE, 0xff, 0x80, 0x27);
+    static bool hasInitialized = false;
+    if (hasInitialized)
+        return;
+    
+    _registerColor(COLOR_BLACK, 0x00, 0x00, 0x00);
+    _registerColor(COLOR_WHITE, 0xff, 0xff, 0xff);
+    _registerColor(COLOR_RED, 0xff, 0x00, 0x00);
+    _registerColor(COLOR_GREEN, 0x00, 0xff, 0x00);
+    _registerColor(COLOR_BLUE, 0x00, 0x00, 0xff);
+    _registerColor(COLOR_YELLOW, 0xff, 0xff, 0x00);
+    _registerColor(COLOR_ORANGE, 0xff, 0x80, 0x27);
+    
+    hasInitialized = true;
 }
 
 bool createColorTableFile(const char* filePath, size_t pixelDataSize,
-						  Color_t (*pFunc)(uint32_t), bool overwrite) {
-	bool isFileExists = access(filePath, F_OK) == 0;
+                          Color_t (*pFunc)(uint32_t), bool overwrite) {
+    bool isFileExists = access(filePath, F_OK) == 0;
 
-	if (isFileExists && !overwrite) {
-		return false;
-	}
+    if (isFileExists && !overwrite) {
+        return false;
+    }
 
-	uint64_t colorTableLength = pow(2, pixelDataSize * 8);
-	ColorTable_t colorTable = _createColorTable(colorTableLength, pFunc);
+    uint64_t colorTableLength = pow(2, pixelDataSize * 8);
+    ColorTable_t colorTable = _createColorTable(colorTableLength, pFunc);
 
-	FILE* outputFile = fopen(filePath, "w");
-	if (outputFile == NULL) {
-		free(colorTable);
-		return false;
-	}
+    FILE* outputFile = fopen(filePath, "w");
+    if (outputFile == NULL) {
+        free(colorTable);
+        return false;
+    }
 
-	fwrite(colorTable, sizeof(Color_t), colorTableLength, outputFile);
-	fclose(outputFile);
-	free(colorTable);
-	return true;
+    fwrite(colorTable, sizeof(Color_t), colorTableLength, outputFile);
+    fclose(outputFile);
+    free(colorTable);
+    return true;
 }
 
 ColorTable_t loadColorTableFile(const char* filePath, size_t pixelDataSize) {
-	FILE* inputFile = fopen(filePath, "r");
-	if (inputFile == NULL)
-		return NULL;
+    FILE* inputFile = fopen(filePath, "r");
+    if (inputFile == NULL)
+        return NULL;
 
-	uint64_t colorTableLength = pow(2, pixelDataSize * 8);
-	ColorTable_t colorTable = _createColorTable(colorTableLength, NULL);
+    uint64_t colorTableLength = pow(2, pixelDataSize * 8);
+    ColorTable_t colorTable = _createColorTable(colorTableLength, NULL);
 
-	fread(colorTable, sizeof(Color_t), colorTableLength, inputFile);
+    fread(colorTable, sizeof(Color_t), colorTableLength, inputFile);
 
-	fclose(inputFile);
-	return colorTable;
+    fclose(inputFile);
+    return colorTable;
 }
 
 
 void _registerColor(Color_t color, uint8_t r, uint8_t g, uint8_t b) {
-	Rgba_t rgba;
-	rgba.r = r;
-	rgba.g = g;
-	rgba.b = b;
-	colorToRgb565DataTable[color] = rgbaToRgb565Data(rgba);
+    Rgba_t rgba;
+    rgba.r = r;
+    rgba.g = g;
+    rgba.b = b;
+    colorToRgb565DataTable[color] = rgbaToRgb565Data(&rgba);
 }
 
 ColorTable_t _createColorTable(uint64_t length, Color_t (*pFunc)(uint32_t)) {
-	ColorTable_t colorTable;
-	uint64_t i = 0;
+    ColorTable_t colorTable;
+    uint64_t i = 0;
 
-	colorTable = (ColorTable_t)malloc(length * sizeof(Color_t));
-	if (pFunc)
-		for (i = 0; i < length; ++i)
-			colorTable[i] = (*pFunc)(i);
+    colorTable = (ColorTable_t)malloc(length * sizeof(Color_t));
+    if (pFunc)
+        for (i = 0; i < length; ++i)
+            colorTable[i] = (*pFunc)(i);
 
-	return colorTable;
+    return colorTable;
 }
--- color.h
+++ color.h
@@ -4,50 +4,46 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define COLOR_USE_TYPE_CHECKING     true
+
 typedef enum {
-	COLOR_BLACK,
-	COLOR_WHITE,
-	COLOR_RED,
-	COLOR_GREEN,
-	COLOR_BLUE,
-	COLOR_YELLOW,
-	COLOR_ORANGE,
-	SIZE_OF_COLOR
+    COLOR_BLACK,
+    COLOR_WHITE,
+    COLOR_RED,
+    COLOR_GREEN,
+    COLOR_BLUE,
+    COLOR_YELLOW,
+    COLOR_ORANGE,
+    SIZE_OF_COLOR
 } Color_t;
 
-typedef struct {
-	union {
-		struct {
-			uint8_t a;
-			uint8_t b;
-			uint8_t g;
-			uint8_t r;
-		};
-		uint32_t data;
-	};
+typedef union {
+    struct {
+        uint8_t a;
+        uint8_t b;
+        uint8_t g;
+        uint8_t r;
+    };
+    uint32_t data;
 } Rgba_t;
 
-typedef struct {
-	union {
-		struct {
-			uint16_t b : 5;
-			uint16_t g : 6;
-			uint16_t r : 5;
-		};
-		uint16_t data;
-	};
+typedef union {
+    struct {
+        uint16_t b : 5;
+        uint16_t g : 6;
+        uint16_t r : 5;
+    };
+    uint16_t data;
 } Rgb565_t;
 
-typedef struct {
-	union {
-		struct {
-			uint16_t b : 5;
-			uint16_t a : 1;
-			uint16_t g : 5;
-			uint16_t r : 5;
-		};
-		uint16_t data;
-	};
+typedef union {
+    struct {
+        uint16_t b : 5;
+        uint16_t a : 1;
+        uint16_t g : 5;
+        uint16_t r : 5;
+    };
+    uint16_t data;
 } Rgab5515_t;
 
 typedef Color_t* ColorTable_t;
@@ -55,27 +51,64 @@ typedef Color_t* ColorTable_t;
 
 uint16_t colorToRgb565DataTable[SIZE_OF_COLOR];
 
-#define rgb565ToRgbaData(rgb565)		((uint32_t)(rgb565).r << 27) | \
-										((uint32_t)(rgb565).g << 18) | \
-										((uint32_t)(rgb565).b << 11)
-#define rgab5515ToRgbaData(rgab5515)	((uint32_t)(rgab5515).r << 27) | \
-										((uint32_t)(rgab5515).g << 19) | \
-										((uint32_t)(rgab5515).b << 11) | \
-										((uint32_t)(rgab5515).a)
-#define rgbaToRgb565Data(rgba)			(((uint16_t)(rgba).r & 0xf8) << 8) |\
-										(((uint16_t)(rgba).g & 0xfc) << 3) |\
-										(((uint16_t)(rgba).b) >> 3)
-#define rgbaToRgab5515Data(rgba)		(((uint16_t)(rgba).r & 0xf8) << 8) |\
-										(((uint16_t)(rgba).g & 0xf8) << 4) |\
-										(((uint16_t)(rgba).a) << 5) |\
-										(((uint16_t)(rgba).b) >> 3)
+#if COLOR_USE_TYPE_CHECK
+    static inline uint32_t rgb565ToRgbaData(Rgb565_t* pRgb565) {
+        return ((uint32_t)pRgb565->r << 27) |
+               ((uint32_t)pRgb565->g << 18) |
+               ((uint32_t)pRgb565->b << 11);
+    }
 
-bool createColorTableFile(const char* filePath, size_t pixelDataSize,
-						  Color_t (*pFunc)(uint32_t), bool overwrite);
-ColorTable_t loadColorTableFile(const char* filePath, size_t pixelDataSize);
+    static inline uint32_t rgab5515ToRgbaData(Rgab5515_t* pRgab5515) {
+        return ((uint32_t)pRgab5515->r << 27) |
+               ((uint32_t)pRgab5515->g << 19) |
+               ((uint32_t)pRgab5515->b << 11) |
+               ((uint32_t)pRgab5515->a);
+    }
+
+    static inline uint16_t rgbaToRgb565Data(Rgba_t* pRgba) {
+        return (((uint16_t)pRgba->r & 0xf8) << 8) |
+               (((uint16_t)pRgba->g & 0xfc) << 3) |
+               (((uint16_t)pRgba->b) >> 3);
+    }
+    
+    static inline uint16_t rgbaToRgab5515Data(Rgba_t* pRgba) {
+        return (((uint16_t)pRgba->r & 0xf8) << 8) |
+               (((uint16_t)pRgba->g & 0xf8) << 4) |
+               (((uint16_t)pRgba->a) << 5) |
+               (((uint16_t)pRgba->b) >> 3);
+    }
+
+    static inline Color_t getColorFromTable(ColorTable_t colorTable,
+                                            uint32_t pixelData) {
+        return colorTable[pixelData];
+    }
+
+    static inline uint16_t colorToRgb565Data(Color_t color) {
+        return colorToRgb565DataTable[color];
+    }
+#else
+    #define rgb565ToRgbaData(pRgb565)   ((uint32_t)pRgb565->r << 27) | \
+                                        ((uint32_t)pRgb565->g << 18) | \
+                                        ((uint32_t)pRgb565->b << 11)
+    #define rgab5515ToRgbaData(pRgab5515)   ((uint32_t)pRgab5515->r << 27) | \
+                                            ((uint32_t)pRgab5515->g << 19) | \
+                                            ((uint32_t)pRgab5515->b << 11) | \
+                                            ((uint32_t)pRgab5515->a)
+    #define rgbaToRgb565Data(pRgba)        (((uint16_t)pRgba->r & 0xf8) << 8) |\
+                                        (((uint16_t)pRgba->g & 0xfc) << 3) |\
+                                        (((uint16_t)pRgba->b) >> 3)
+    #define rgbaToRgab5515Data(pRgba)   (((uint16_t)pRgba->r & 0xf8) << 8) |\
+                                        (((uint16_t)pRgba->g & 0xf8) << 4) |\
+                                        (((uint16_t)pRgba->a) << 5) |\
+                                        (((uint16_t)pRgba->b) >> 3)
+    #define getColorFromTable(colorTable, pixelData)    colorTable[pixelData]
+    #define colorToRgb565Data(color)    colorToRgb565DataTable[color]
+#endif
+
 void initColorLib(void);
 
-#define getColorFromTable(colorTable, pixelData)	colorTable[pixelData]
-#define colorToRgb565Data(color)		colorToRgb565DataTable[color]
+bool createColorTableFile(const char* filePath, size_t pixelDataSize,
+                          Color_t (*pFunc)(uint32_t), bool overwrite);
+ColorTable_t loadColorTableFile(const char* filePath, size_t pixelDataSize);
 
 #endif // __COLOR_H__
