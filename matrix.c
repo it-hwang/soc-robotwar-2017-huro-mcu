f65f8139dcf0362de292ade08b848839d2713108
HeoHeo <h0h6h2h5@gmail.com>
2017-07-23 01:17:13 +0900

    Fix typo

--- matrix.c
+++ matrix.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 
 #include "matrix.h"
@@ -15,7 +16,7 @@ Matrix8_t* createMatrix8(uint16_t width, uint16_t height) {
 
 Matrix16_t* createMatrix16(uint16_t width, uint16_t height) {
     Matrix16_t* pMatrix = (Matrix16_t*)malloc(sizeof(Matrix16_t));
-    size_t size = height * width * sizeof(uint8_t);
+    size_t size = height * width * sizeof(uint16_t);
     pMatrix->elements = (uint16_t*)malloc(size);
     pMatrix->width = width;
     pMatrix->height = height;
@@ -33,7 +34,7 @@ Matrix32_t* createMatrix32(uint16_t width, uint16_t height) {
     return pMatrix;
 }
 
-Matrix8_t* cloneMatrix8(Matrix8_t pMatrix8) {
+Matrix8_t* cloneMatrix8(Matrix8_t* pMatrix8) {
     uint16_t width = pMatrix8->width;
     uint16_t height = pMatrix8->height;
     Matrix8_t* pMatrix = createMatrix8(width, height);
@@ -43,7 +44,7 @@ Matrix8_t* cloneMatrix8(Matrix8_t pMatrix8) {
     return pMatrix;
 }
 
-Matrix16_t* cloneMatrix16(Matrix16_t pMatrix16) {
+Matrix16_t* cloneMatrix16(Matrix16_t* pMatrix16) {
     uint16_t width = pMatrix16->width;
     uint16_t height = pMatrix16->height;
     Matrix16_t* pMatrix = createMatrix16(width, height);
@@ -53,7 +54,7 @@ Matrix16_t* cloneMatrix16(Matrix16_t pMatrix16) {
     return pMatrix;
 }
 
-Matrix32_t* cloneMatrix32(Matrix32_t pMatrix32) {
+Matrix32_t* cloneMatrix32(Matrix32_t* pMatrix32) {
     uint16_t width = pMatrix32->width;
     uint16_t height = pMatrix32->height;
     Matrix32_t* pMatrix = createMatrix32(width, height);
--- matrix.h
+++ matrix.h
@@ -25,9 +25,9 @@ typedef struct {
 Matrix8_t* createMatrix8(uint16_t width, uint16_t height);
 Matrix16_t* createMatrix16(uint16_t width, uint16_t height);
 Matrix32_t* createMatrix32(uint16_t width, uint16_t height);
-Matrix8_t* cloneMatrix8(Matrix8_t pMatrix8);
-Matrix16_t* cloneMatrix16(Matrix16_t pMatrix16);
-Matrix32_t* cloneMatrix32(Matrix32_t pMatrix32);
+Matrix8_t* cloneMatrix8(Matrix8_t* pMatrix8);
+Matrix16_t* cloneMatrix16(Matrix16_t* pMatrix16);
+Matrix32_t* cloneMatrix32(Matrix32_t* pMatrix32);
 void destroyMatrix8(Matrix8_t* pMatrix8);
 void destroyMatrix16(Matrix16_t* pMatrix16);
 void destroyMatrix32(Matrix32_t* pMatrix32);
