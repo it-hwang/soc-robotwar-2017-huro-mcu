944779ce74f35c2b8287f352ca4b647f46d93d2b
nadara9209 <nadara9209@gmail.com>
2017-08-03 08:13:38 +0900

    Modify_8/3

--- robot_protocol.h
+++ robot_protocol.h
@@ -1,6 +1,17 @@
 #ifndef __ROBOT_PROTOCOL_H__
 #define __ROBOT_PROTOCOL_H__
 
+#define MOTION_DEFAULT 0x34
+#define MOTION_MOVE_RIGHT 0x36
+#define MOTION_MOVE_LEFT 0x38
+#define MOTION_TURN_LEFT 0x3a
+#define MOTION_TURN_RIGHT 0x3c
+#define MOTION_TURN_CORNER 0x3e
+#define MOTION_MOVE_FORWARD 0x40
+#define MOTION_MOVE_DOWN 0x48
+#define MOTION_TURN_LEFT_BIG 0x53
+#define MOTION_TURN_RIGHT_BIG 0x54
+
 int openRobotPort(void);
 void closeRobotPort(void);
 void sendDataToRobot(short data);
