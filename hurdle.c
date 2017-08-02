e366e5e33f86ab85e6613ce2dc38c3eda8b6d817
INTAEK HWANG <hitn8752@gmail.com>
2017-08-03 01:03:02 +0900

    Arrange code line

--- hurdle.c
+++ hurdle.c
@@ -66,48 +66,48 @@ bool hurdleMain(void) {
 
 ObjectList_t* _captureBlueObject(Screen_t* pScreen, Color_t color, bool flg) {
         
-        readFpgaVideoData(pScreen);
+    readFpgaVideoData(pScreen);
 
-        Matrix8_t* pColorMatrix = createColorMatrix(pScreen, 
-                                    pColorTables[color]);
-        if(flg){
-            applyDilationToMatrix8(pColorMatrix, 1);
-            applyErosionToMatrix8(pColorMatrix, 2);
-            applyDilationToMatrix8(pColorMatrix, 1);
-        }
-        
-        ObjectList_t* objList = detectObjectsLocation(pColorMatrix);
-       
-        destroyMatrix8(pColorMatrix);
+    Matrix8_t* pColorMatrix = createColorMatrix(pScreen, 
+                                pColorTables[color]);
+    if(flg){
+        applyDilationToMatrix8(pColorMatrix, 1);
+        applyErosionToMatrix8(pColorMatrix, 2);
+        applyDilationToMatrix8(pColorMatrix, 1);
+    }
+    
+    ObjectList_t* objList = detectObjectsLocation(pColorMatrix);
+    
+    destroyMatrix8(pColorMatrix);
 
-        if (objList != NULL) {
-            int i;
-            for(i = 0; i < objList->size; ++i) {
-                int x;
-                int y;
-                Object_t object = objList->list[i];
-                PixelData_t* pixels = pScreen->elements;
+    if (objList != NULL) {
+        int i;
+        for(i = 0; i < objList->size; ++i) {
+            int x;
+            int y;
+            Object_t object = objList->list[i];
+            PixelData_t* pixels = pScreen->elements;
 
-                for(y = object.minY; y <= object.maxY; ++y) {
-                    for(x = object.minX; x <= object.maxX; ++x) {
-                        int index = y * pScreen->width + x;
-                        uint16_t* pOutput = (uint16_t*)&pixels[index];
+            for(y = object.minY; y <= object.maxY; ++y) {
+                for(x = object.minX; x <= object.maxX; ++x) {
+                    int index = y * pScreen->width + x;
+                    uint16_t* pOutput = (uint16_t*)&pixels[index];
 
-                        if(y == object.minY || y == object.maxY) {
-                            *pOutput = 0xF800;
-                        } else if(x == object.minX || x == object.maxX) {
-                            *pOutput = 0xF800;
-                        }
+                    if(y == object.minY || y == object.maxY) {
+                        *pOutput = 0xF800;
+                    } else if(x == object.minX || x == object.maxX) {
+                        *pOutput = 0xF800;
                     }
                 }
-
-                int index = (int)object.centerY * pScreen->width + (int)object.centerX;
-                uint16_t* pOutput = (uint16_t*)&pixels[index];
-                *pOutput = 0x1F;
             }
+
+            int index = (int)object.centerY * pScreen->width + (int)object.centerX;
+            uint16_t* pOutput = (uint16_t*)&pixels[index];
+            *pOutput = 0x1F;
         }
+    }
 
-        displayScreen(pScreen);
+    displayScreen(pScreen);
 
-        return objList;
+    return objList;
 }
