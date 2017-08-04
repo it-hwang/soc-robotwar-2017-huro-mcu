6b8e077e113597e8f24c99ee51c211e6e9bf1745
INTAEK HWANG <hitn8752@gmail.com>
2017-08-04 22:49:21 +0900

    Change robot protocol

--- robot_protocol.c
+++ robot_protocol.c
@@ -4,21 +4,9 @@
 #include "robot_protocol.h"
 #include "uart_api.h"
 
-#define _UART_BAUD_RATE		57600
+#define _UART_BAUD_RATE		9600
 #define _UART_BITS			8
 #define _UART_STOPS			1
-
-typedef struct _BIOLOID_PACKET {
-	unsigned char startCode0;	// start code 0			(1byte)
-	unsigned char startCode1;	// start code 1			(1byte)
-	unsigned char dataLow0;		// low data				(1byte)
-	unsigned char dataLow1; 	// inverted low data	(1byte)
-	unsigned char dataHigh0;	// high data			(1byte)
-	unsigned char dataHigh1;	// inverted high data	(1byte)
-} BIOLOID_PACKET;
-
-BIOLOID_PACKET* _createPacket(void);
-
 
 int openRobotPort(void) {
 	int status;
@@ -44,25 +32,18 @@ void DelayLoop(int delay_time)
 
 void Send_Command(unsigned char Ldata)
 {
-	unsigned char Command_Buffer[6] = {0,};
-	
-	unsigned char Ldata1 = ~Ldata;
+	unsigned char Command_Buffer[1] = {0,};
 
-	Command_Buffer[0] = 0xff;	// Start Byte -> 0xff
-	Command_Buffer[1] = 0x55; // Start Byte1 -> 0x55
-    Command_Buffer[2] = Ldata;
-	Command_Buffer[3] = Ldata1;
-	Command_Buffer[4] = 0x00;  // 0x00
-	Command_Buffer[5] = 0xff; // 0xff
+	Command_Buffer[0] = Ldata;
 
-	uart1_buffer_write(Command_Buffer, 6);
+	uart1_buffer_write(Command_Buffer, 1);
 }
 
 void waitMotion(void)
 {
-	unsigned char Command_Buffer[6] = {0,};
+	unsigned char Command_Buffer[1] = {0,};
 	
-	uart1_buffer_read(Command_Buffer, 6);
+	uart1_buffer_read(Command_Buffer, 1);
 }
 
 #define ERROR	0
