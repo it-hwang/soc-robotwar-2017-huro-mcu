e02a6a6ba18cee3296ec92c3986f607111d9cffd
HeoHeo <h0h6h2h5@gmail.com>
2017-08-09 17:22:36 +0900

    Add functions

--- robot_protocol.c
+++ robot_protocol.c
@@ -8,17 +8,6 @@
 #define _UART_BITS			8
 #define _UART_STOPS			1
 
-typedef struct _BIOLOID_PACKET {
-	unsigned char startCode0;	// start code 0			(1byte)
-	unsigned char startCode1;	// start code 1			(1byte)
-	unsigned char dataLow0;		// low data				(1byte)
-	unsigned char dataLow1; 	// inverted low data	(1byte)
-	unsigned char dataHigh0;	// high data			(1byte)
-	unsigned char dataHigh1;	// inverted high data	(1byte)
-} BIOLOID_PACKET;
-
-BIOLOID_PACKET* _createPacket(void);
-
 
 int openRobotPort(void) {
 	int status;
@@ -36,6 +25,111 @@ void closeRobotPort(void) {
 	uart_close();
 }
 
+void sendDataToRobot(uint8_t data) {
+	unsigned char buffer[1];
+	buffer[0] = data;
+
+	uart1_buffer_write(buffer, 1);
+}
+
+uint8_t receiveDataFromRobot(void) {
+	unsigned char buffer[1] = {0};
+	uart1_buffer_read(buffer, 1);
+
+	return buffer[0];
+}
+
+
+bool runMotion(uint8_t motionId, bool wait) {
+	sendDataToRobot(motionId);
+
+	if (wait == true) {
+		return waitMotion();
+	}
+	else {
+		return true;
+	}
+}
+
+bool waitMotion(void) {
+	uint8_t receivedData = receiveDataFromRobot();
+	return true;
+}
+
+
+// BUG: 현재 머리 각도가 최초 한번밖에 조절이 안되는 문제가 있다.
+// RoboBasic 코드의 SERVO 명령어가 제대로 작동하는지가 가장 의심된다.
+void setHeadVertical(int degrees) {
+	if (degrees < -90 || degrees > 90)
+		return;
+
+	int offset = degrees + 100;
+
+	sendDataToRobot(1);
+	receiveDataFromRobot();
+	sendDataToRobot(17);
+	receiveDataFromRobot();
+	sendDataToRobot(offset);
+	waitMotion();
+}
+
+void setHeadHorizontal(int degrees) {
+	if (degrees < -90 || degrees > 90)
+		return;
+
+	int offset = degrees + 100;
+
+	sendDataToRobot(1);
+	receiveDataFromRobot();
+	sendDataToRobot(11);
+	receiveDataFromRobot();
+	sendDataToRobot(offset);
+	waitMotion();
+}
+
+void setHead(int horizontalDegrees, int verticalDegrees) {
+	setHeadHorizontal(horizontalDegrees);
+	setHeadVertical(verticalDegrees);
+}
+
+
+// TODO: 함수 내용을 작성해야한다.
+// 실험용이므로 거리가 정확하지 않아도 된다.
+// 일단은 milliMeters 신경쓰지 않고 전진보행_1만 써서 구현해도 된다.
+//  - 전진보행_1: 두 걸음 전진 (7cm)
+//  - 전진보행_2: 네 걸음 전진 (16cm)
+//  - 짧은전진보행: 한 걸음 전진 (3cm)
+//  - 짧은전진보행2: 한 걸음 전진 (2.5cm)
+//  - 짧은전진보행3: 한 걸음 전진 (2cm)
+//  - 짧은전진보행4: 한 걸음 전진 (1.5cm)
+//  - 빠른전진보행_1: 두 걸음 전진 (7cm)
+//  - 전진보행_빠른_바리케이드: 빠른전진보행으로 길게 전진 (40cm)
+//  - 전진보행_빠른_바리케이드_멀리: 빠른전진보행으로 길게 전진 (56cm)
+bool walkForward(int milliMeters) {
+
+}
+
+bool walkBackward(int milliMeters) {
+
+}
+
+bool walkLeft(int milliMeters) {
+
+}
+
+bool walkRight(int milliMeters) {
+
+}
+
+bool turnLeft(int degrees) {
+
+}
+
+bool turnRight(int degrees) {
+
+}
+
+
 void DelayLoop(int delay_time)
 {
 	while(delay_time)
@@ -50,47 +144,3 @@ void Send_Command(unsigned char Ldata)
 
 	uart1_buffer_write(Command_Buffer, 1);
 }
-
-void waitMotion(void)
-{
-	unsigned char Command_Buffer[1] = {0,};
-	
-	uart1_buffer_read(Command_Buffer, 1);
-	printf("%d\n", Command_Buffer[0]);
-}
-
-void setHeadVertical(int angle) {
-	if (angle < -90 || angle > 90)
-		return;
-
-	int offset = angle + 100;
-
-	Send_Command(1);
-	waitMotion();
-	Send_Command(17);
-	waitMotion();
-	Send_Command(offset);
-	waitMotion();
-}
-
-void setHeadHorizontal(int angle) {
-	if (angle < -90 || angle > 90)
-		return;
-
-	int offset = angle + 100;
-
-	Send_Command(1);
-	waitMotion();
-	Send_Command(11);
-	waitMotion();
-	Send_Command(offset);
-	waitMotion();
-}
-
-void setHead(int horizontalAngle, int verticalAngle) {
-	setHeadHorizontal(horizontalAngle);
-	setHeadVertical(verticalAngle);
-}
-
-#define ERROR	0
-#define OK	1
--- robot_protocol.h
+++ robot_protocol.h
@@ -1,15 +1,31 @@
 #ifndef __ROBOT_PROTOCOL_H__
 #define __ROBOT_PROTOCOL_H__
 
+#include <stdint.h>
+#include <stdbool.h>
+
 int openRobotPort(void);
 void closeRobotPort(void);
-void sendDataToRobot(short data);
+void sendDataToRobot(uint8_t data);
+uint8_t receiveDataFromRobot(void);
+
+bool runMotion(uint8_t motionId, bool wait);
+bool waitMotion(void);
+
+void setHeadVertical(int degrees);
+void setHeadHorizontal(int degrees);
+void setHead(int horizontalDegrees, int verticalDegrees);
+
+bool walkForward(int milliMeters);
+bool walkBackward(int milliMeters);
+bool walkLeft(int milliMeters);
+bool walkRight(int milliMeters);
+bool turnLeft(int degrees);
+bool turnRight(int degrees);
+
+// 삭제 예정
 void DelayLoop(int delay_time);
 void Send_Command(unsigned char Ldata);
-void waitMotion(void);
-void setHeadVertical(int angle);
-void setHeadHorizontal(int angle);
-void setHead(int horizontalAngle, int verticalAngle);
 
 #endif // __ROBOT_PROTOCOL_H__
 
