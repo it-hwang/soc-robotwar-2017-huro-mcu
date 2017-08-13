4de1a389529d93b22c5c378e280ae34f1a95935a
HeoHeo <h0h6h2h5@gmail.com>
2017-08-14 00:18:43 +0900

    Add functions

--- robot_protocol.h
+++ robot_protocol.h
@@ -5,25 +5,25 @@
 #include <stdbool.h>
 
 enum {
-    ROBOT_SET_MOTOR,
-    ROBOT_SET_SPEED,
-    ROBOT_GET_MOTOR,
+    ROBOT_SET_SERVO_OFFSET = 1,
+    ROBOT_SET_SERVO_SPEED,
+    ROBOT_GET_SERVO_OFFSET,
     ROBOT_TURN_OFF_ARM_MOTORS,
     MOTION_BASIC_STANCE,
     MOTION_BOARD_CONNECTING_STANCE,
     MOTION_CHECK_SIDELINE_STANCE,
     MOTION_HEAD_FRONT,
     MOTION_HEAD_BOTTOM,
-    ROBOT_WALK_FRONT,
-    ROBOT_WALK_FRONT_FAST,
-    ROBOT_WALK_FRONT_SHORT1,
-    ROBOT_WALK_FRONT_SHORT2,
-    ROBOT_WALK_FRONT_SHORT3,
-    ROBOT_WALK_BACK,
-    ROBOT_WALK_FRONT_QUICK_LOW,
-    ROBOT_WALK_FRONT_QUICK,
-    ROBOT_WALK_FRONT_QUICK_THRESHOLD,
-    ROBOT_WALK_BACK_QUICK,
+    ROBOT_WALK_FORWARD,
+    ROBOT_WALK_FORWARD_FAST,
+    ROBOT_WALK_FORWARD_SHORT1,
+    ROBOT_WALK_FORWARD_SHORT2,
+    ROBOT_WALK_FORWARD_SHORT3,
+    ROBOT_WALK_BACKWARD,
+    ROBOT_WALK_FORWARD_QUICK_LOW,
+    ROBOT_WALK_FORWARD_QUICK,
+    ROBOT_WALK_FORWARD_QUICK_THRESHOLD,
+    ROBOT_WALK_BACKWARD_QUICK,
     ROBOT_WALK_CRAWL,
     MOTION_MOVE_LEFT_LIGHT,
     MOTION_MOVE_LEFT_MIDDLE,
@@ -59,11 +59,15 @@ uint8_t receiveDataFromRobot(void);
 
 bool runMotion(uint8_t motionId, bool wait);
 bool waitMotion(void);
+void setServoSpeed(uint8_t speed);
+void setServoOffset(uint8_t servoId, uint8_t offset);
+uint8_t getServoOffset(uint8_t servoId);
 
 void setHeadVertical(int degrees);
 void setHeadHorizontal(int degrees);
 void setHead(int horizontalDegrees, int verticalDegrees);
 
+bool runWalk(uint8_t walkId, uint8_t steps);
 bool walkForward(int millimeters);
 bool walkBackward(int millimeters);
 bool walkLeft(int millimeters);
