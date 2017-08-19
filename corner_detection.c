6103c493ef53208d47ca99beb36307d9fcfafda6
INTAEK HWANG <hitn8752@gmail.com>
2017-08-19 23:34:04 +0900

    Add _lookAround() & _captureBothSide()

--- corner_detection.c
+++ corner_detection.c
@@ -46,6 +46,157 @@ bool cornerDetection(void) {
     return true;
 }
 
+static int _lookAround() {
+    static const char* LOG_FUNCTION_NAME = "_captureAround()";
+    static const int INIT_STATE = -1;
+    static const int LIMIT_TRY_COUNT = 2;
+
+    int resultState = INIT_STATE;
+    int tryCount = 0;
+    
+    while(tryCount < LIMIT_TRY_COUNT) {
+        resultState = _captureBothSide();
+
+        if(resultState == CAPTURE_ERROR)
+            tryCount++;
+        else 
+            break;
+    }
+
+    return resultState;
+}
+
+static int _captureBothSide(void) {
+    
+    Screen_t* pScreen = createDefaultScreen();
+
+    _setHeadLeft();
+    Line_t* leftLine = _captureLeftLine(pScreen);
+
+    _setHeadRight();
+    Line_t* rightLine = _captureRightLine(pScreen);
+
+    destroyScreen(pScreen);
+
+    if(leftLine == NULL && rightLine != NULL) {
+        free(rightLine);
+        return LEFT_SIDE_CLEAR;
+    }
+
+    if(leftLine != NULL && rightLine == NULL) {
+        free(leftLine);
+        return RIGHT_SIDE_CLEAR;
+    }
+
+    if(leftLine != NULL && rightLine != NULL) {
+        free(leftLine);
+        free(rightLine);
+        return NO_CLEAR_SIDE;
+    }
+
+    return CAPTURE_ERROR;  
+}
+
+static void _setHeadRight(void) {
+    setServoSpeed(30);
+    runMotion(MOTION_CHECK_SIDELINE_STANCE);
+    setHead(85, -50);
+    mdelay(1000);
+    resetServoSpeed();
+}
+
+static void _setHeadLeft(void) {
+    setServoSpeed(30);
+    runMotion(MOTION_CHECK_SIDELINE_STANCE);
+    setHead(-85, -50);
+    mdelay(1000);
+    resetServoSpeed();
+}
+
+static void _setStandardStand(void) {
+    setServoSpeed(30);
+    runMotion(MOTION_BASIC_STANCE);
+    setHead(0, 0);
+    mdelay(1000);
+    resetServoSpeed();
+}
+
+static Line_t* _captureRightLine(Screen_t* pScreen) {
+    
+    readFpgaVideoDataWithWhiteBalance(pScreen);
+
+    Matrix16_t* pSubMatrix = createSubMatrix16(pScreen, 70, 0, 89, 110);
+
+    Matrix8_t* pColorMatrix = createColorMatrix(pSubMatrix, 
+                                pColorTables[COLOR_BLACK]);
+
+    applyFastDilationToMatrix8(pColorMatrix, 1);
+    applyFastErosionToMatrix8(pColorMatrix, 2);
+    applyFastDilationToMatrix8(pColorMatrix, 1);
+
+    Line_t* returnLine = lineDetection(pColorMatrix);
+
+    drawColorMatrix(pSubMatrix, pColorMatrix);
+    overlapMatrix16(pSubMatrix, pScreen, 70, 0);
+
+    _drawLine(pScreen, returnLine, 70, 0);
+    displayScreen(pScreen);
+
+    destroyMatrix8(pColorMatrix);
+    destroyMatrix16(pSubMatrix);
+
+    return returnLine;
+}
+
+static Line_t* _captureLeftLine(Screen_t* pScreen) {
+
+    readFpgaVideoDataWithWhiteBalance(pScreen);
+
+    Matrix16_t* pSubMatrix = createSubMatrix16(pScreen, 90, 0, 109, 110);
+
+    Matrix8_t* pColorMatrix = createColorMatrix(pSubMatrix, 
+                                pColorTables[COLOR_BLACK]);
+
+    applyFastDilationToMatrix8(pColorMatrix, 1);
+    applyFastErosionToMatrix8(pColorMatrix, 2);
+    applyFastDilationToMatrix8(pColorMatrix, 1);
+
+    Line_t* returnLine = lineDetection(pColorMatrix);
+
+    drawColorMatrix(pSubMatrix, pColorMatrix);
+    overlapMatrix16(pSubMatrix, pScreen, 90, 0);
+
+    _drawLine(pScreen, returnLine, 90, 0);
+    displayScreen(pScreen);
+
+    destroyMatrix8(pColorMatrix);
+    destroyMatrix16(pSubMatrix);
+
+return returnLine;
+}
+
+static void _drawLine(Screen_t* pScreen, Line_t* pLine, int minX, int minY) {
+    
+    PixelData_t* pixels = pScreen->elements;
+
+    
+    int centerX = (int)pLine->centerPoint.x + minX;
+
+    for(int x = minX; x <= centerX; ++x) {
+        int y = (int)pLine->leftPoint.y + minY;
+        int index = y * pScreen->width + x;
+        uint16_t* pOutput = (uint16_t*)&pixels[index];
+        *pOutput = 0xF800;
+    }
+
+    for(int x = minX + pLine->rightPoint.x; x >= centerX; --x) {
+        int y = (int)pLine->rightPoint.y + minY;
+        int index = y * pScreen->width + x;
+        uint16_t* pOutput = (uint16_t*)&pixels[index];
+        *pOutput = 0xF800;
+    }
+}
+
 bool cornerDetection(void) {
 
     _pDefaultScreen = createDefaultScreen();
