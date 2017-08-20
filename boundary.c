6bbb14ebc281da887fd2a1462457597ccdfb44fe
INTAEK HWANG <hitn8752@gmail.com>
2017-08-20 20:36:00 +0900

    Add establishBoundary()

---
+++ boundary.c
@@ -0,0 +1,47 @@
+
+#include "matrix.h"
+#include "object_detection.h"
+#include "log.h"
+
+Matrix8_t* establishBoundary(Matrix8_t* pColorMatrix) {
+    static const char* LOG_FUNCTION_NAME = "establishBoundary()";
+
+    Matrix16_t* pLabelMatrix = createMatrix16(pColorMatrix->width, pColorMatrix->height);
+    memset(pLabelMatrix->elements, 0, (pLabelMatrix->height * pLabelMatrix->width) * sizeof(uint16_t));
+
+    ObjectList_t* pObjectList = detectObjectsLocationWithLabeling(pColorMatrix, pLabelMatrix);
+
+    Object_t* pObject = _getBoundaryObject(pObjectList, pLabelMatrix);
+
+    Matrix8_t* pReturnMatrix = _traceBoundaryLine(pObject, pLabelMatrix);
+
+    _fillBoundary(pReturnMatrix);
+
+    if(pObjectList != NULL){
+        free(pObjectList->list);
+        free(pObjectList);
+    }
+    
+    destroyMatrix16(pLabelMatrix);
+
+    return pReturnMatrix;
+}
+
+void applyBoundary(Screen_t* pScreen, Matrix8_t* pBoundaryMatrix) {
+    static const char* LOG_FUNCTION_NAME = "applyBoundary()";
+    
+    if(pScreen->width != pBoundaryMatrix->width)
+        return;
+    
+    if(pScreen->height != pBoundaryMatrix->height)
+        return;
+
+    int width = pScreen->width;
+    int height = pScreen->height;
+    int length = width * height;
+
+    for(int i = 0; i < length; ++i) {
+        if(pBoundaryMatrix->elements[i] != 0xFF)
+            pScreen->elements[i] = 0x7bcf; //NONE_COLOR
+    }
+}
---
+++ boundary.h
@@ -0,0 +1,5 @@
+#ifndef __BOUNDARY_H__
+#define __BOUNDARY_H__
+
+
+#endif // __BOUNDARY_H__
