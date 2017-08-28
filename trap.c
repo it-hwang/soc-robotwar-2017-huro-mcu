3ea1347962a17091850f7acf0a3429eef17cf911
INTAEK HWANG <hitn8752@gmail.com>
2017-08-28 19:30:38 +0900

    Add _establishBoundary()

--- trap.c
+++ trap.c
@@ -1,19 +1,43 @@
 
-#include "trap.h"
-#include "boundary.h"
 
-static bool _approachTrap(void);
+#include "trap.h"
+#include "graphic_interface.h"
+#include "robot_protocol.h"
+#include "white_balance.h"
+#include "image_filter.h"
+#include "object_detection.h"
+#include "boundary.h"
+#include "color.h"
+#include "log.h"
+#include "debug.h"
+
+static Object_t* _searchTrap(Screen_t* pScreen);
+static Object_t* _candidateObjectForBoundary(pScreen);
+static void _establishBoundary(Screen_t* pScreen);
+static void _setBoundary(Screen_t* pScreen, Object_t* pObject);
+static bool _isTrapObject(Screen_t* pScreen,  &pTrapObject);
+static int _measureObjectDistance(pTrapObject);
+static void _approachObject(int distance);
 
 bool trapMain(void) {
-    _approachTrap();
+
+    _searchTrap();
+
+    return false;
 }
 
-static Object_t* _searchTrap(Screen_t* pScreen) {
+static Object_t* _searchTrap(void) {
 
     const int MAX_TRIES = 10;
+    
+    const int APPROACH_DISTANCE = 20;
+    
+    const int APPROACH_DISTANCE_ERROR = 30;
 
-    for(int i = 0; i < MAX_TRIES; ++i) {
-        Object_t* pObject = _candidateObjectForBoundary(pSceen);
+    Screen_t* pScreen = createDefualtScreen();
+    
+    for(int nTries = 0; nTries < MAX_TRIES; ++i) {
+        Object_t* pObject = _candidateObjectForBoundary(pScreen);
 
         if(pObject == NULL)
             continue;
@@ -23,103 +47,69 @@ static Object_t* _searchTrap(Screen_t* pScreen) {
         free(pObject);
 
         Object_t* pTrapObject = NULL;
-        bool isTrap = _isTrapObject(pScreen, &pTrapObject);
+        bool isTrap = _isTrapObject(pScreen, pTrapObject);
     
-        if( !isTrap ) {
-            
-        }
+        if( isTrap )
+            return pTrapObject;
+        
+        int distance = _measureObjectDistance(pTrapObject);
+
+        free(pTrapObject);
+
+        if(distance < APPROACH_DISTANCE + APPROACH_DISTANCE_ERROR)
+            break;
+
+        nTries = 0;
+
+        _approachObject(distance);
     }
     
-    return pObject;
+    return NULL;
 }
 
-static bool _approachTrap(void) {
-    // 함정을 발견하지 못할 경우 다시 찍는 횟수
-    const int MAX_TRIES = 10;
-    
-    // 장애물에 다가갈 거리 (밀리미터)
-    const int APPROACH_DISTANCE = 20;
-    // 거리 허용 오차 (밀리미터)
-    const int APPROACH_DISTANCE_ERROR = 30;
+static Object_t* _candidateObjectForBoundary(pScreen) {
 
-    const int ALIGN_OFFSET_X = 4;
-    const double MILLIMETERS_PER_PIXELS = 2.;
-    
-    int nTries;
-    for (nTries = 0; nTries < MAX_TRIES; ++nTries) {
-        bool hasFound;
-        
-        // 앞뒤 정렬
-        int distance = measureTrapDistance();
-        hasFound = (distance != 0);
-        if (!hasFound)
-            continue;
-        
-        if (distance > APPROACH_DISTANCE + APPROACH_DISTANCE_ERROR) {
-            printDebug("전진보행으로 이동하자. (거리: %d)\n", distance);
-            walkForward(distance - APPROACH_DISTANCE);
-            mdelay(300);
-            nTries = 0;
-            continue;
-        }
-        
-        printDebug("접근 완료.\n");
-        break;
-    }
-    if (nTries >= MAX_TRIES) {
-        printDebug("시간 초과!\n");
-        return false;
-    }
-    
-    // 달라붙어 비비기
-    runWalk(ROBOT_WALK_FORWARD_QUICK, 12);
-    runWalk(ROBOT_WALK_FORWARD_QUICK_THRESHOLD, 4);
+    readFpgaVideoDataWithWhiteBalance(pScreen);
 
-    for (nTries = 0; nTries < MAX_TRIES; ++nTries) {
-        bool hasFound;
-        
-        // 앞뒤 정렬
-        int distance = measureTrapDistance();
-        hasFound = (distance != 0);
-        if (!hasFound)
-            continue;
-        
-        if (distance > APPROACH_DISTANCE + APPROACH_DISTANCE_ERROR) {
-            printDebug("종종걸음으로 이동하자. (거리: %d)\n", distance);
-            runWalk(ROBOT_WALK_FORWARD_QUICK_THRESHOLD, 4);
-            mdelay(300);
-            nTries = 0;
-            continue;
-        }
-        
-        // 좌우 정렬
-        int offsetX = _measureGreenBridgeCenterOffsetX();
-        if (offsetX < ALIGN_OFFSET_X * -1) {
-            int millimeters = abs(offsetX) * MILLIMETERS_PER_PIXELS;
-            printDebug("offsetX: %d, Go left.\n", offsetX);
-            walkLeft(millimeters);
-            mdelay(300);
-            nTries = 0;
-            continue;
-        }
-        else if (offsetX > ALIGN_OFFSET_X) {
-            int millimeters = abs(offsetX) * MILLIMETERS_PER_PIXELS;
-            printDebug("offsetX: %d, Go right.\n", offsetX);
-            walkRight(millimeters);
-            mdelay(300);
-            nTries = 0;
-            continue;
-        }
-        
-        printDebug("접근 완료.\n");
-        break;
-    }
-    if (nTries >= MAX_TRIES) {
-        printDebug("시간 초과!\n");
-        return false;
-    }
+    _establishBoundary(pScreen);
 
-    runWalk(ROBOT_WALK_FORWARD_QUICK_THRESHOLD, 4);
+}
+
+static void _establishBoundary(Screen_t* pScreen) {
+
+    Matrix8_t pYellowMatrix = createColorMatrix(pScreen, pColorTables[COLOR_YELLOW]);
+    Matrix8_t pWhiteMatrix = createColorWhite(pScreen, pColorTables[COLOR_WHITE]);
+
+    Matrix8_t* pMergedColorMatrix = 
+    overlapColorMatrix(pYellowMatrix, pWhiteMatrix);
+
+    applyFastErosionToMatrix8(pMergedColorMatrix, 1);
+    applyFastDilationToMatrix8(pMergedColorMatrix, 2);
+    applyFastErosionToMatrix8(pMergedColorMatrix, 1);
     
-    return true;
+    Matrix8_t* pBoundaryMatrix = establishBoundary(pMergedColorMatrix);
+
+    applyBoundary(pScreen, pBoundaryMatrix);
+
+    destroyMatrix8(pWhiteColorMatrix);
+    destroyMatrix8(pYellowMatrix);
+    destroyMatrix8(pMergedColorMatrix);
+    destroyMatrix8(pBoundaryMatrix);
+
+}
+
+static void _setBoundary(Screen_t* pScreen, Object_t* pObject) {
+
+}
+
+static bool _isTrapObject(Screen_t* pScreen,  &pTrapObject) {
+    return false;
+}
+
+static int _measureObjectDistance(pTrapObject) {
+    return 0;
+}
+
+static void _approachObject(int distance) {
+
 }
